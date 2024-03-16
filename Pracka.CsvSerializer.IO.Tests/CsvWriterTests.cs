ff8fccdf16ff0e717feace784275d0f8212cff07
Stefik, Ondrej <ondrej.stefik@gmail.com>
2024-03-16 14:16:27 +0100

    path is more readable

--- Pracka.CsvSerializer.IO.Tests/CsvWriterTests.cs
+++ Pracka.CsvSerializer.IO.Tests/CsvWriterTests.cs
@@ -6,27 +6,12 @@ namespace Pracka.CsvSerializer.IO.Tests
     {
         private static string GetAbsolutePathFrom(string relativePath)
         {
-            return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativePath);
+            var combinedPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativePath);
+            return Path.GetFullPath(combinedPath);
         }
 
         [Fact]
         public async Task File_Created_Successfully()
-        {
-            var fullPath = GetAbsolutePathFrom("./filePath.csv");
-            Assert.False(File.Exists(fullPath), "File should not be existing yet");
-
-            using (var csvWriter = new CsvWriter(fullPath))
-            {
-                await csvWriter.WriteEntityAsync(new TestEntity());
-            }
-
-            Assert.True(File.Exists(fullPath), "File should now exist");
-            File.Delete(fullPath);
-            Assert.False(File.Exists(fullPath), "File should be no longer existing");
-        }
-
-        [Fact]
-        public async Task Test_File_Creation()
         {
             var fullPath = GetAbsolutePathFrom("./filePath.csv");
             Assert.False(File.Exists(fullPath), "File should not be existing yet");
